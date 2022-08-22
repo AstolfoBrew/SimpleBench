3410db040405d9cc8162e291e526fd55337f0b22
fxeP1 <64731916+fxeP1@users.noreply.github.com>
2022-08-22 21:17:57 +0100

    Formatting and SW Platform support
    (Formatted most of the lua files because the formatting looked pretty ugly, it shouldn't effect anything in the test)
    
    [+] Script-Ware platform support (should detect Mac and iOS)

--- RBXExecutorInfo.lua
+++ RBXExecutorInfo.lua
@@ -7,102 +7,114 @@
 -- https://github.com/AstolfoBrew/SimpleBench
 ------------- CODE
 local http_request = http_request or request or (http and http.request) or (syn and syn.request)
-local exploit_type = 'Generic'
-if getexecutorname then
-  exploit_type = getexecutorname() -- Script-Ware is known to use this, however others might too - should return exec name
-elseif identifyexecutor then
-  exploit_type = identifyexecutor() -- Script-Ware is known to use this, however others might too - should return exec name
+local exploit_type = "Generic"
+
+local identifyexecutor = identifyexecutor or getexecutorname
+if identifyexecutor then
+	local exploit_name, exploit_version = identifyexecutor()
+	if exploit_name == "ScriptWare" then
+		--// Check for Script-Ware to ensure the correct platform is being tested.
+		if exploit_version == "Mac" then
+			exploit_type = "ScriptWare Mac"
+		elseif exploit_version == "iOS" then
+			exploit_type = "ScriptWare iOS"
+		else
+			exploit_type = "ScriptWare Windows"
+		end
+	else
+		exploit_type = exploit_name
+	end
 elseif KRNL_LOADED then
-  exploit_type = 'KRNL'
+	exploit_type = "KRNL"
 elseif PROTOSMASHER_LOADED then
-  exploit_type = 'ProtoSmasher'
+	exploit_type = "ProtoSmasher"
 elseif is_sirhurt_closure then
-  exploit_type = 'Pedohurt'
+	exploit_type = "Pedohurt"
 elseif SENTINEL_LOADED then
-  exploit_type = 'Sentinel'
+	exploit_type = "Sentinel"
 elseif syn then
-  exploit_type = 'Synapse-X'
-elseif hookfunction_raw and hmjdfk then
-  exploit_type = 'Fluxus Mac Free' -- dk if fluxus mac free still exists; don't think it does
-  if not getconnections then
-    getgenv().getconnections = function() return {} end
-    getgenv().set_thread_context = function() return end
-    getgenv().fluxus_loaded = true
-  end
+	exploit_type = "Synapse-X"
 elseif FLUXUS_LOADED then
-  exploit_type = 'Fluxus'
+	exploit_type = "Fluxus"
 end
 --
-local rawinfo = {['name'] = exploit_type; ['http'] = http_request; ['hwid'] = nil; ['id'] = nil}
-local info = rawinfo;
+local rawinfo = { ["name"] = exploit_type, ["http"] = http_request, ["hwid"] = nil, ["id"] = nil }
+local info = rawinfo
 if setmetatable then
-  local hwid;
-  local id;
-  local clientInfo;
-  local hwidHeaders = {'Sw-Fingerprint'; 'Fingerprint'; 'Syn-Fingerprint'};
-  local idHeaders = {
-    'Sw-User-Identifier';
-    'User-Identifier';
-    'Syn-User-Identifier';
-    (unpack or table.unpack and (unpack or table.unpack)(hwidHeaders));
-  };
-  local getClientInfo = function()
-    if not clientInfo then clientInfo = rawinfo.http({Url = 'https://httpbin.org/get'}) end
-    return clientInfo
-  end
-  info = setmetatable({}, {
-    __index = function(t, k)
-      k = string.lower(k)
-      if rawinfo[k] then return rawinfo[k] end
-      -------------- HWID
-      if k == 'hwid' then
-        if hwid then
-          return hwid
-        else
-          if not string.find or not string.lower then
-            error('{SimpleBench Exec Info} ~ Cannot search string without string.find and string.lower')
-          end
-          local Body = getClientInfo().Body;
-          local ParsedBody = typeof(Body) == 'string' and game:GetService 'HttpService':JSONDecode(Body) or Body
-          for h, v in pairs(ParsedBody.headers) do
-            if (string.find(string.lower(h), '-fingerprint')) then
-              hwid = v;
-              return hwid;
-            end
-            for _, o in pairs(hwidHeaders) do
-              if string.lower(h) == string.lower(o) then
-                hwid = v
-                return hwid
-              end
-            end
-          end
-          return hwid
-        end
-      end
-      -------------- EXEC USER ID
-      if k == 'id' then
-        if id then return id end
-        if not string.find or not string.lower then
-          error('{SimpleBench Exec Info} ~ Cannot search string without string.find and string.lower')
-        end
-        local Body = getClientInfo().Body;
-        local ParsedBody = typeof(Body) == 'string' and game:GetService 'HttpService':JSONDecode(Body) or Body
-        local Headers = ParsedBody.Headers;
-        for h, v in pairs(Headers) do
-          if (string.find(string.lower(h), '-user-identifier')) then
-            id = v;
-            return v;
-          end
-          for _, o in pairs(idHeaders) do
-            if string.lower(h) == string.lower(o) then
-              id = v
-              return v
-            end
-          end
-        end
-        return id
-      end
-    end;
-  })
+	local hwid
+	local id
+	local clientInfo
+	local hwidHeaders = { "Sw-Fingerprint", "Fingerprint", "Syn-Fingerprint" }
+	local idHeaders = {
+		"Sw-User-Identifier",
+		"User-Identifier",
+		"Syn-User-Identifier",
+		(unpack or table.unpack and (unpack or table.unpack)(hwidHeaders)),
+	}
+	local getClientInfo = function()
+		if not clientInfo then
+			clientInfo = rawinfo.http({ Url = "https://httpbin.org/get" })
+		end
+		return clientInfo
+	end
+	info = setmetatable({}, {
+		__index = function(t, k)
+			k = string.lower(k)
+			if rawinfo[k] then
+				return rawinfo[k]
+			end
+			-------------- HWID
+			if k == "hwid" then
+				if hwid then
+					return hwid
+				else
+					if not string.find or not string.lower then
+						error("{SimpleBench Exec Info} ~ Cannot search string without string.find and string.lower")
+					end
+					local Body = getClientInfo().Body
+					local ParsedBody = typeof(Body) == "string" and game:GetService("HttpService"):JSONDecode(Body)
+						or Body
+					for h, v in pairs(ParsedBody.headers) do
+						if string.find(string.lower(h), "-fingerprint") then
+							hwid = v
+							return hwid
+						end
+						for _, o in pairs(hwidHeaders) do
+							if string.lower(h) == string.lower(o) then
+								hwid = v
+								return hwid
+							end
+						end
+					end
+					return hwid
+				end
+			end
+			-------------- EXEC USER ID
+			if k == "id" then
+				if id then
+					return id
+				end
+				if not string.find or not string.lower then
+					error("{SimpleBench Exec Info} ~ Cannot search string without string.find and string.lower")
+				end
+				local Body = getClientInfo().Body
+				local ParsedBody = typeof(Body) == "string" and game:GetService("HttpService"):JSONDecode(Body) or Body
+				local Headers = ParsedBody.Headers
+				for h, v in pairs(Headers) do
+					if string.find(string.lower(h), "-user-identifier") then
+						id = v
+						return v
+					end
+					for _, o in pairs(idHeaders) do
+						if string.lower(h) == string.lower(o) then
+							id = v
+							return v
+						end
+					end
+				end
+				return id
+			end
+		end,
+	})
 end
 return info
--- RBXExecutorLoader.lua
+++ RBXExecutorLoader.lua
@@ -6,95 +6,129 @@
 -- License: MIT
 -- https://github.com/AstolfoBrew/SimpleBench
 ------------- DEFAULTS
-local Settings = {['RBXWaitAfterRun'] = false; ['Iterations'] = 50; ['Branch'] = 'Release'; ['Silent'] = false};
+local Settings = { ["RBXWaitAfterRun"] = false, ["Iterations"] = 50, ["Branch"] = "Release", ["Silent"] = false }
 ------------- CODE
 if not string or not string.split then
-  error('{SimpleBench-Loader} ~ Missing string?.split - Please use the regular SimpleBench.')
+	error("{SimpleBench-Loader} ~ Missing string?.split - Please use the regular SimpleBench.")
 end
 if not string or not string.lower then
-  error('{SimpleBench-Loader} ~ Missing string?.lower - Please use the regular SimpleBench.')
+	error("{SimpleBench-Loader} ~ Missing string?.lower - Please use the regular SimpleBench.")
 end
-if not loadstring then error('{SimpleBench-Loader} ~ Missing loadstring - Please use the regular SimpleBench.') end
-if not game then error('{SimpleBench-Loader} ~ Missing game - Please run this in a Roblox Executor Environment') end
+if not loadstring then
+	error("{SimpleBench-Loader} ~ Missing loadstring - Please use the regular SimpleBench.")
+end
+if not game then
+	error("{SimpleBench-Loader} ~ Missing game - Please run this in a Roblox Executor Environment")
+end
 
-local Args = {...};
+local Args = { ... }
 if Args[1] then
-  for k, v in pairs(Args[1]) do
-    if typeof(Settings[k]) == typeof(v) or typeof(Settings[k]) == 'nil' then
-      Settings[k] = v;
-    else
-      error('Invalid Settings Type: Expected ' .. typeof(Settings[k]) .. ', got ' .. typeof(v) .. ' for setting ' .. k);
-    end
-  end
+	for k, v in pairs(Args[1]) do
+		if typeof(Settings[k]) == typeof(v) or typeof(Settings[k]) == "nil" then
+			Settings[k] = v
+		else
+			error(
+				"Invalid Settings Type: Expected "
+					.. typeof(Settings[k])
+					.. ", got "
+					.. typeof(v)
+					.. " for setting "
+					.. k
+			)
+		end
+	end
 end
-_G.SimpleBenchSettings = Settings;
-local Repo = Args[2] or 'AstolfoBrew/SimpleBench';
-local RepoSplit = {'AstolfoBrew'; 'SimpleBench'};
-if string.split then RepoSplit = string.split(Repo, '/') end
-local RepoOwner = RepoSplit[1] or 'AstolfoBrew';
-local RepoName = RepoSplit[2] or 'SimpleBench';
-local ExecInfoUrl = string.format('https://raw.githubusercontent.com/%s/%s/%s/RBXExecutorInfo.lua', RepoOwner, RepoName,
-                                  (Settings.Branch == 'Release' or string.sub(Settings.Branch, 1, 1) == 'v') and 'main' or
-                                      Settings.Branch);
-local ExecInfoSrc = game:HttpGetAsync(ExecInfoUrl);
-local ScriptUrl = Settings.Branch == 'Release' and
-                      string.format('https://github.com/%s/%s/releases/latest/download/SimpleBench.lua', RepoOwner, RepoName) or
-                      string.sub(Settings.Branch, 1, 1) == 'v' and
-                      string.format('https://github.com/%s/%s/releases/download/%s/SimpleBench.lua', RepoOwner, RepoName,
-                                    string.sub(Settings.Branch, 2, #Settings.Branch)) or
-                      string.format('https://raw.githubusercontent.com/%s/%s/%s/SimpleBench.lua', RepoOwner, RepoName,
-                                    Settings.Branch);
-local ScriptSrc = game:HttpGetAsync(ScriptUrl);
+_G.SimpleBenchSettings = Settings
+local Repo = Args[2] or "AstolfoBrew/SimpleBench"
+local RepoSplit = { "AstolfoBrew", "SimpleBench" }
+if string.split then
+	RepoSplit = string.split(Repo, "/")
+end
+local RepoOwner = RepoSplit[1] or "AstolfoBrew"
+local RepoName = RepoSplit[2] or "SimpleBench"
+local ExecInfoUrl = string.format(
+	"https://raw.githubusercontent.com/%s/%s/%s/RBXExecutorInfo.lua",
+	RepoOwner,
+	RepoName,
+	(Settings.Branch == "Release" or string.sub(Settings.Branch, 1, 1) == "v") and "main" or Settings.Branch
+)
+local ExecInfoSrc = game:HttpGetAsync(ExecInfoUrl)
+local ScriptUrl = Settings.Branch == "Release"
+		and string.format("https://github.com/%s/%s/releases/latest/download/SimpleBench.lua", RepoOwner, RepoName)
+	or string.sub(Settings.Branch, 1, 1) == "v" and string.format(
+		"https://github.com/%s/%s/releases/download/%s/SimpleBench.lua",
+		RepoOwner,
+		RepoName,
+		string.sub(Settings.Branch, 2, #Settings.Branch)
+	)
+	or string.format("https://raw.githubusercontent.com/%s/%s/%s/SimpleBench.lua", RepoOwner, RepoName, Settings.Branch)
+local ScriptSrc = game:HttpGetAsync(ScriptUrl)
 
-local ExecInfoSrcChunk, Err = loadstring(ExecInfoSrc, 'RBXExecutorInfo.lua');
-local ExecInfo;
-if typeof(ExecInfoSrcChunk) ~= 'function' then
-  warn('Error getting Executor Information Chunkname: ', Err or ExecInfoSrcChunk)
-  ExecInfo = {['name'] = 'Unknown'};
+local ExecInfoSrcChunk, Err = loadstring(ExecInfoSrc, "RBXExecutorInfo.lua")
+local ExecInfo
+if typeof(ExecInfoSrcChunk) ~= "function" then
+	warn("Error getting Executor Information Chunkname: ", Err or ExecInfoSrcChunk)
+	ExecInfo = { ["name"] = "Unknown" }
 else
-  ExecInfo = ExecInfoSrcChunk();
+	ExecInfo = ExecInfoSrcChunk()
 end
 
-if _ENV then getgenv = function() return _ENV end end
-if getfenv and not getgenv then getgenv = function() return getfenv(0) end end
+if _ENV then
+	getgenv = function()
+		return _ENV
+	end
+end
+if getfenv and not getgenv then
+	getgenv = function()
+		return getfenv(0)
+	end
+end
 
-local oldPrint = print;
-local oldWarn = warn;
-local oldError = error;
-local log = '';
+local oldPrint = print
+local oldWarn = warn
+local oldError = error
+local log = ""
 if getgenv and writefile then
-  pcall(function()
-    getgenv().print = function(...)
-      log = log .. (table.concat({...}, '\t')) .. '\n';
-      return oldPrint(...)
-    end
-    getgenv().warn = function(...)
-      log = log .. (table.concat({'WARN:'; ...}, '\t')) .. '\n';
-      return oldWarn(...)
-    end
-    getgenv().error = function(...)
-      log = log .. (table.concat({'ERROR:'; ...}, '\t')) .. '\n';
-      return oldError(...)
-    end
-  end)
+	pcall(function()
+		getgenv().print = function(...)
+			log = log .. (table.concat({ ... }, "\t")) .. "\n"
+			return oldPrint(...)
+		end
+		getgenv().warn = function(...)
+			log = log .. (table.concat({ "WARN:", ... }, "\t")) .. "\n"
+			return oldWarn(...)
+		end
+		getgenv().error = function(...)
+			log = log .. (table.concat({ "ERROR:", ... }, "\t")) .. "\n"
+			return oldError(...)
+		end
+	end)
 end
 
-local FinalScore = loadstring(ScriptSrc, 'SimpleBench.lua')();
+local FinalScore = loadstring(ScriptSrc, "SimpleBench.lua")()
 
 if writefile then
-  writefile('SimpleBench.log',
-            string.format('Final Score:\n%s\n(Lua Version: %s - Executor: %s - Script from %s)\n\n------ LOGS ------\n\n%s',
-                          tostring(FinalScore), _VERSION or 'UNKNOWN', ExecInfo.name, ScriptUrl, log));
+	writefile(
+		"SimpleBench.log",
+		string.format(
+			"Final Score:\n%s\n(Lua Version: %s - Executor: %s - Script from %s)\n\n------ LOGS ------\n\n%s",
+			tostring(FinalScore),
+			_VERSION or "UNKNOWN",
+			ExecInfo.name,
+			ScriptUrl,
+			log
+		)
+	)
 end
 
 if getgenv and writefile then
-  pcall(function()
-    getgenv().print = oldPrint;
-    getgenv().warn = oldWarn;
-    getgenv().error = oldError
-  end)
+	pcall(function()
+		getgenv().print = oldPrint
+		getgenv().warn = oldWarn
+		getgenv().error = oldError
+	end)
 end
 
-_G.SimpleBenchSettings = nil;
+_G.SimpleBenchSettings = nil
 
-return FinalScore;
+return FinalScore
